a9c4d3bc27f6453a0c4034f61153e0814245345d
Jheorgenes Warlley <jheorgenes@gmail.com>
2024-10-30 17:21:23 -0300

    -- comentando Model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 // inserir manualmente essa importação e apelide ela de Authenticable
 use Illuminate\Foundation\Auth\User as Authenticable;
 
+/** Definindo para usar a autenticação do Laravel */
 class User extends Authenticable
 {
     //
